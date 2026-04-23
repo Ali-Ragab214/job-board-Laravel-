fdac351dcb9f27a76110cec503cfe99c05d36415
Ali Ragab <alighonim78@gmail.com>
2026-04-24 02:21:34 +0300

    migrations

---
+++ database/migrations/2026_04_23_221105_create_candidates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('resume', 255)->nullable();
+            $table->string('location', 255)->nullable();
+            $table->decimal('experience_years', 3, 1)->nullable();
+            $table->string('phone', 20)->nullable();
+            $table->string('headline', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('candidates');
+    }
+};
---
+++ database/migrations/2026_04_23_222055_create_jobs_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      Schema::create('jobs', function (Blueprint $table) {
+    $table->id();
+
+    $table->foreignId('employer_id')
+        ->constrained('employers')
+        ->onDelete('cascade');
+
+    $table->foreignId('category_id')
+        ->nullable()
+        ->constrained('job_categories')
+        ->nullOnDelete();
+
+    $table->string('title');
+    $table->string('slug')->unique();
+
+    $table->text('description');
+    $table->text('responsibilities')->nullable();
+    $table->text('qualifications')->nullable();
+
+    $table->decimal('salary_min', 10, 2)->nullable();
+    $table->decimal('salary_max', 10, 2)->nullable();
+
+    $table->string('location')->nullable();
+
+    $table->enum('work_type', [
+        'full_time', 'part_time', 'contract', 'temporary', 'internship'
+    ])->default('full_time');
+
+    $table->enum('experience_level', [
+        'entry', 'mid', 'senior', 'director', 'executive'
+    ])->default('entry');
+
+    $table->enum('status', ['open', 'closed'])->default('open');
+
+    $table->date('application_deadline')->nullable();
+
+    $table->unsignedBigInteger('views_count')->default(0);
+
+    $table->timestamps();
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
